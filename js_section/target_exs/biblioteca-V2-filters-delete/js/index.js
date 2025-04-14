975d956d63e59c61f56ecab51ff23ffa80e7b292
Mariana Alves <m.bourdain.alves>
2025-04-14 12:32:13 +0100

    Ex. done

---
+++ js_section/target_exs/biblioteca-V2-filters-delete/js/index.js
@@ -0,0 +1,78 @@
+//?DEFINE LOCAL VARIS.
+let grid = document.getElementById('grid');
+let filters = document.getElementById('filters')
+
+//changeEvent = muda a cada introdução de caracter (melhor para forms)
+//inputEvent = alterar o valor do input (input type = text; textarea...)
+
+//?APP EVENTS
+filters.addEventListener('click', filterEvents, false);
+filters.addEventListener('input', filterEvents, false);
+grid.addEventListener('click', gridEvents, false)
+
+//?BUSINESS LOGIC
+
+// 1 - Add a filter section, so the user can filter books by already read, not read, all, and by the title / author of the book.
+function filterEvents(e){
+    let el = e.target;
+
+    if(el.id === 'allBtn'){
+        showBooks(getBooks());
+    }
+
+    if(el.id === 'readBtn'){
+        showBooks(getReadBooks());
+    }
+
+    if(el.id === 'notReadBtn'){
+        showBooks(getNotReadBooks());
+    }
+
+    if((el.id === 'searchTxt') && (e.type === 'input')){
+        let text = el.value.toLowerCase();
+        showBooks(getBooksByTitle(el.value));
+    }
+}
+
+function gridEvents(e){
+    console.log(e)
+    if((e.target.nodeName === 'P') && (e.target.textContent.search('✅') > -1)){
+        showBooks(getReadBooks());
+    }
+
+    if((e.target.nodeName === 'P') && (e.target.textContent.search('❌') > -1)){
+        showBooks(getNotReadBooks());
+    }
+
+    if (e.target.dataset.type = 'deleteBtn'){
+        showBooks(deleteBook(e.target.dataset.idbook));
+    }
+}
+
+
+//?LISTENERS
+console.log(livros);
+showBooks(getBooks());
+
+
+function showBooks(arrayBooks){
+    grid.innerHTML = '';
+
+    arrayBooks.map( book => {
+        grid.innerHTML += `
+            <article>
+                <h1>${book.title}</h1>
+                <h2>${book.author}</h2>
+                <img src="livros/${book.imageUrl}" alt="${book.title}}">
+                <p>Already read: ${book.alreadyRead ? '✅' : '❌' }  </p>
+                <button class="btn" data-type=deleteBtn data-idbook=${book.id}>Delete</button>
+                <button class="btn" data-type=editBtn data-idbook=${book.id}>Edit</button>
+            </article>
+        `;
+    })
+}
+
+// 2 - Inside each card, add a delete btn to delete each book
+
+
+
---
+++ js_section/target_exs/biblioteca-V2-filters-delete/js/utils.js
@@ -0,0 +1,23 @@
+/*  Bilioteca-2
+ 1 - acrescentar uma seccao,  de filtros, para poder filtrar por ja lidos, nao lidos, todos, e por titulo ou autor do livtro
+ 2 - dentro de cada card, acrescentar um botao de delete, para apagar o respectivo livro
+ 3 - ao clicar no thumbnail mostrear popup
+*/
+
+/// metodo ler todos os livros
+const getBooks = () => livros;
+
+// metodo para mostrar livros ja lidos
+const getReadBooks = () => livros.filter( livro => livro.alreadyRead === true);
+
+// metodo para mostrar livros nao lidos
+const getNotReadBooks = () => livros.filter( livro => livro.alreadyRead === false);
+
+//Pesquisa pelo título
+const getBooksByTitle = (text) => livros.filter( livro => {
+    let title = livro.title.toLowerCase();
+    return title.search(text) > -1;
+});
+
+//Delete do book pr id
+const deleteBook = (id) => livros = livros.filter ( livro => livro.id != id);
